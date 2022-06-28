b84a24cb04904aadb4f483046ac751367b38f1f9
hendrik.kausch <kausch@se-aachen.de>
2022-06-28 13:50:26 +0200

    update keychecker

--- bin/keyreport.py
+++ bin/keyreport.py
@@ -1,18 +1,21 @@
 import bibtexparser
+import re
 from os import listdir
 from os.path import isfile, join
 
+
 def getmdkeys(path):
-    with open(path) as mbse:
-        mbse_data = mbse.read()
+    with open(path) as file:
+        file_data = file.read()
 
-    start = mbse_data.find('keys')
-    end = mbse_data.find('---', start)
+    file_keys = re.findall(r'keys: (\[[A-Za-z]+\+?[0-9]+[\,\n \w\+]*\])', file_data)
 
-    all_keys = mbse_data[start+6:end]
-    res = all_keys.replace('\n', '').replace(' ', '').strip('][').split(',')
-
-    return res
+    res = []
+    for string in file_keys:
+        lists = string.strip('][').replace(' ', '').replace('\n', '').split(',')
+        res.append(lists)
+    flat_list = [x for xs in res for x in xs]
+    return flat_list
 
 
 def getbibkeys(path):
@@ -31,24 +34,27 @@ def getbibkeys(path):
 def checkkeys(mdkeys, bibkeys):
     missing_keys = []
     for key in mdkeys:
-         if (not (key in bibkeys)):
-             missing_keys.append(key)
+        if (not (key in bibkeys)):
+            missing_keys.append(key)
     return missing_keys
 
 
-def main(filepath,bibpath):
+def main(filepath, bibpath, outname):
     files = [f for f in listdir(filepath) if isfile(join(filepath, f))]
-    bib_keys = getbibkeys(bibpath+'/all-software-engineering-rwth-references.bib')
-    bib_keys_new = getbibkeys(bibpath+'/additional-bib-entries.bib')
+    bib_keys = getbibkeys(bibpath + '/all-software-engineering-rwth-references.bib')
+    bib_keys_new = getbibkeys(bibpath + '/additional-bib-entries.bib')
     bib_keys.extend(bib_keys_new)
-    with open("keyreport.md", "w") as text_file:
+    with open(outname+".md", "w") as text_file:
         for file in files:
-            keys = getmdkeys(filepath+'/'+file)
+            keys = getmdkeys(filepath + '/' + file)
             missing = checkkeys(keys, bib_keys)
-            text_file.write('### Missing keys from '+file+':\n')
+            text_file.write('### Missing keys from ' + file + ':\n')
             for key in missing:
-                text_file.write('* '+key+'\n')
-            text_file.write('\n')
+                text_file.write('* ' + key + '\n')
+            text_file.write('\n'+'Checked keys ' + file + ':')
+            text_file.write(str(keys) + '\n')
+
 
 if __name__ == "__main__":
-    main('_pages/research','assets/bibliography')
+    main('_pages/research', 'assets/bibliography', 'research')
+    main('_pages', 'assets/bibliography', 'pages')
